141f87a4e6128de95b4d27cba934a564ded4fc7a
JSulthoni <sulthonijavier@gmail.com>
2023-12-02 22:42:57 +0700

    hardcoded backend URL

--- src/utils/makeRequest.js
+++ src/utils/makeRequest.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-    baseURL : import.meta.env.VITE_BACKEND_URL,
+    baseURL : 'https://bndlr.cyclic.app',
     headers: { Authorization: 'Bearer ' + import.meta.env.VITE_MONGO_API_KEY }
 });
